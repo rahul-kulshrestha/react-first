e910de06de8de4355d71bec4686dbfe8aca3eb7d
Rahul Kulshrestha <rahulkulshrestha011@gmail.com>
2022-07-07 16:13:08 +0530

    after hosting

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Rahul and Arti host thanks 
         </a>
       </header>
     </div>
